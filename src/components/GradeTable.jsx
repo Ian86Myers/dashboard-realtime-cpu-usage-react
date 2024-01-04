d5ea5e791526145906b8fe48386a7a5c0efef58f
Ian Myers <ian.myers@ibm.com>
2024-01-04 12:26:46 -0500

    Install ArrowDropDown for Netlify

--- src/components/GradeTable.jsx
+++ src/components/GradeTable.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 // import AdminIcon from "./adminIcon";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import Box from "@mui/material/Box";
